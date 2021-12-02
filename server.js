94823c8a4366ce451a2acddcb1bbfc75550b2bed
doublebyte1 <info@doublebyte.net>
2021-12-02 23:58:51 +0100

    - catchup with latest changes from tg2g api

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ var HttpStatus = require('http-status-codes');
 var express = require('express');
 var telegram = require('telegram-bot-api');
 
-const stockOnly = true;
+const stockOnly = false;
 const userId = process.env.TGTG_USER_ID;
 //const userToken = process.env.TGTG_USER_TOKEN;
 const telegramToken = process.env.TELEGRAM_API_TOKEN;
 const email = process.env.TGTG_EMAIL;
 const password = process.env.TGTG_PWD;
+const access_token = process.env.TGTG_TOKEN;
 let oldStr = "";
 
 const url='https://apptoogoodtogo.com/api';
@@ -26,13 +27,13 @@ var api = new telegram({
 });
 const chatId = -363155949
 
-
+/*
 function loginByEmail(){
 
     var request = require('request');
     var options = {
       'method': 'POST',
-      'url': `${url}/auth/v1/loginByEmail`,
+      'url': `${url}/auth/v2/loginByEmail`,
       'headers': {
         'Content-Type': 'application/json'
       },
@@ -75,72 +76,85 @@ function refreshToken(refresh_token){
     
         setInterval(function() {
         checkFavorites(json.access_token);
-        }, 60 * 1000); // 60 * 1000 milsec
+        //}, 60 * 1000); // 60 * 1000 milsec
+        }, 120 * 1000); // 60 * 1000 milsec
   
 
     });
 
 }
-
-
-
-
-
+*/
 function checkFavorites(access_token){
 
-    var request = require('request');
-    var options = {
-    'method': 'POST',
-    'url': `${url}/item/v6/`,
-    'headers': {
-        'User-Agent': 'TGTG/20.2.2 Dalvik/2.1.0 (Linux; U; Android 9; Moto G Build/PQ1A.181205.006)',
-        'Authorization': `Bearer ${access_token}`,
-        'Content-Type': 'application/json; charset=utf-8',
-        'Host': 'apptoogoodtogo.com'
-    },
-    body: `{\"user_id\":\"${userId}\",\"origin\":{\"latitude\":41.4150093,\"longitude\":2.1579298},\"radius\":5000.0,\"page_size\":100,\"page\":1,\"discover\":false,\"favorites_only\":true,\"item_categories\":[],\"diet_categories\":[],\"pickup_earliest\":null,\"pickup_latest\":null,\"search_phrase\":null,\"with_stock_only\":${stockOnly},\"hidden_only\":false}`
-
+    var axios = require('axios');
+    var data = `{"user_id":"${userId}","origin":{"latitude":41.4150419,"longitude":2.1577982},"radius":30.0,"page_size":100,"page":1,"discover":false,"favorites_only":true,"item_categories":[],"diet_categories":[],"with_stock_only":${stockOnly},"hidden_only":false,"we_care_only":false}`;
+    
+    var config = {
+      method: 'post',
+      url: `${url}/item/v7/`,
+      headers: { 
+        'Cookie': 'datadome=5Mw8-dI-oHWoh6nITLamuWQoVACw2.zlJvWc.5njmP_h60FCeoaNG3RTAnsK.~dwhIeb22BmJiHEfNKYar5pSwL5.PYfvoK11Sb6tC48fD_5x1QXzCjG6bJ-n7THEai; datadome=uknuyh3-BcanNwm9fY3TweX6XgaHrz6X~vWiu8-TS.qNsQllW3TAvo30~hYAs_as47PyB2w9QGGOgaqH64a3hvs.KzQSC0HSF2-2oHLWAQLIE4n2e4K00Hi8zYEgCwX', 
+        'Accept': 'application/json', 
+        'User-Agent': 'TGTG/21.11.1 Dalvik/2.1.0 (Linux; U; Android 9; Moto G Build/PQ1A.181205.006)', 
+        'Accept-Language': 'en-GB', 
+        'Authorization': `Bearer ${access_token}`, 
+        'Content-Type': 'application/json; charset=utf-8', 
+        'Content-Length': '262', 
+        'Host': 'apptoogoodtogo.com', 
+        'Connection': 'close', 
+        'Accept-Encoding': 'gzip, deflate'
+      },
+      data : data
     };
-    request(options, function (error, response) { 
-    if (error) throw new Error(error);
 
-    var json = JSON.parse(response.body); 
-    //console.log(json);
-    if (json.items){
-        const dt = new Date();
-        console.log('{ "timestamp": "' + dt.toUTCString() + '", "status": "' 
-            + HttpStatus.getStatusText(parseInt(response.statusCode)) + '", "items": ' 
-                + json.items.length + '}');
+      axios(config)
+      .then(function (response) {
 
-        if (json.items.length > 0){
-            let str = "<b>Some items are available on:</b>\n";
+        var json = response.data; 
 
-            for(const store of json.items) {
-                //console.log(store['store']);
-                str+='<u>' + store['store']['store_name'] + '</u>\n';
-            }
-
-            if (oldStr !== str){
-                api.sendMessage({
-                    chat_id: chatId,
-                    text: str,
-                    parse_mode: "HTML"
-                })
+         if (json.items){
+            console.log("found items");
+            const dt = new Date();
+            console.log('{ "timestamp": "' + dt.toUTCString() + '", "status": "' 
+                + HttpStatus.getStatusText(parseInt(response.status)) + '", "items": ' 
+                    + json.items.length + '}');
+    
+            if (json.items.length > 0){
+                let str = "<b>Some items are available on:</b>\n";
+    
+                for(const store of json.items) {
+                    //console.log(store['store']);
+                    str+='<u>' + store['store']['store_name'] + '</u>\n';
+                }
+    
+                if (oldStr !== str){
+                    api.sendMessage({
+                        chat_id: chatId,
+                        text: str,
+                        parse_mode: "HTML"
+                    })
+        
+                }
+                oldStr = str;
+    
     
             }
-            oldStr = str;
-
-
-        }
+            
+         }
+         else console.log("no items found"); 
         
-     } 
-    });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
 
 }
 
+
 function fn60sec(access_token) {
     // runs every 60 sec and runs on init.
-        checkFavorites(access_token);
+    checkFavorites(access_token);
     }
 
 
@@ -149,7 +163,12 @@ app.listen(3000, function () {
 
     console.log('Tg2g Notifier listening on port 3000!');
 
-    loginByEmail();
+    fn60sec(access_token);
+    setInterval(fn60sec, 60*1000);
 
+    setInterval(function() {
+    checkFavorites(access_token);
+    //}, 60 * 1000); // 60 * 1000 milsec
+    }, 120 * 1000); // 60 * 1000 milsec
 
 });
